d5bc47394ccb2f454a1f1e8de593b13d8cfc32c7
Dastan <ermekoffdastan@gmail.com>
2023-05-04 16:06:06 +0600

    'add'

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Request
+from typing import Union
+from starlette.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+templates = Jinja2Templates(directory="templates")
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
+
+
+@app.get("/")
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/about")
+def about(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})
+
+
+@app.get("/contact")
+def contact(request: Request):
+    return templates.TemplateResponse("contact.html", {"request": request})
+
+
+@app.get("/pricing")
+def pricing(request: Request):
+    return templates.TemplateResponse("pricing.html", {"request": request})
+
+
+@app.get("/faq")
+def faq(request: Request):
+    return templates.TemplateResponse("faq.html", {"request": request})
